d279c5bd095cf7c1a2c94fc6cb50f48c0927468c
Josecodesalot <josesandwich1@gmail.com>
2019-07-05 02:26:43 -0400

    Design fixes and offline capabilities added

--- app/src/main/java/com/example/dontforgettograbthat/utils/UserClient.java
+++ app/src/main/java/com/example/dontforgettograbthat/utils/UserClient.java
@@ -3,8 +3,15 @@ package com.example.dontforgettograbthat.utils;
 import android.app.Application;
 
 import com.example.dontforgettograbthat.Models.User;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserClient extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);    }
+
     private User user = null;
 
     public User getUser() {
